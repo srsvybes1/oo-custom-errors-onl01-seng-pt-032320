272611894305b0b8f4936d629f90238dd6909223
Cody Toney-Griffith <cody.toney@gmail.com>
2020-05-06 14:33:57 +0000

    Automatically backed up by Learn

--- lib/custom_errors.rb
+++ lib/custom_errors.rb
@@ -9,14 +9,10 @@ class Person
     self.partner = person
     person.partner = self
   end
-class PartnerError < StandardError 
-end 
+class PartnerError < StandardError
+end
 end
 
 beyonce = Person.new("Beyonce")
 beyonce.get_married("Jay-Z")
 puts beyonce.name
-
-
-
-
